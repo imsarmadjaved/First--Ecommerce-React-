b274623f1a2f989bdc02c5cea567ca82b240505c
sarmad153 <sarmadofficial.6@gmail.com>
2026-03-16 12:30:29 +0500

    ok

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import DarkMode from "./Darkmode";
+import DarkMode from "./DarkMode";
 import { IoMdSearch } from "react-icons/io";
 import { FaCartShopping } from "react-icons/fa6";
 import { FaCaretDown } from "react-icons/fa";
